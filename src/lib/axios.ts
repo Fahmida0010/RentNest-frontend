2c6087010123824c65140f98aff91faa577f5ff6
Fahmida0010 <fahmidaakter0010@gmail.com>
2026-08-03 14:59:54 +0600

    axios updated

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL + '/api',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
